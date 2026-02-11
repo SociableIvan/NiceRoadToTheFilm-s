b01db47fda93f79b7b4fe6d9912cf8abde1139bb
solohubivan <solohubivan@gmail.com>
2026-02-11 09:37:53 +0200

    Initial Commit

---
+++ NiceRoadToTheFilm's/App/NiceRoadToTheFilm_sApp.swift
@@ -0,0 +1,17 @@
+//
+//  NiceRoadToTheFilm_sApp.swift
+//  NiceRoadToTheFilm's
+//
+//  Created by Ivan Solohub on 11.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct NiceRoadToTheFilm_sApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NiceRoadToTheFilm's/LoadingScreenView.swift
@@ -0,0 +1,8 @@
+//
+//  LoadingScreenView.swift
+//  NiceRoadToTheFilm's
+//
+//  Created by Ivan Solohub on 11.02.2026.
+//
+
+
